802d41e20dfc3bd2f21e8b4ce4cc735a815ed540
zahi.ebanaya <zahi.ebanaya@spectrumhealth.org>
2018-10-16 16:46:53 -0400

    added a cover photo.  i'm not sure how to add images to entire comp.ts

--- GreatBlackMoms-SPA/src/app/home/home.component.ts
+++ GreatBlackMoms-SPA/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  bckImg = '../../assets/img/cover-photo.jpg';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private renderer: Renderer2) {
   }
 
+  ngOnInit() {
+
+  }
 }
