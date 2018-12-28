999738e291951584c5a664f1b18f6c437c83f8c0
Zahi Ebanaya <zahi.ebanaya@gmail.com>
2018-12-28 11:56:18 -0500

    Replacing the old image
    Old cover photo has been replaced with one of Jordan's choosing.  Made some attempts at formatting the cover photo.

--- GreatBlackMoms-SPA/src/app/home/home.component.ts
+++ GreatBlackMoms-SPA/src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+  // styles: ['body { background: url(\'http://placehold.it/1920x1080\') no-repeat center center fixed; -webkit-background-size: cover;' +
+  //  '-moz-background-size: cover; background-size: cover; -o-background-size: cover;}']
 })
 export class HomeComponent implements OnInit {
 
